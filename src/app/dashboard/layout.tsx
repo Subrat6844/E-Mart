5348889802bb13e524c73b59462c671b6943a43f
Subrat <chaudharysubrat78@outlook.com>
2024-12-08 17:41:24 +0530

    page loader added for admin panel

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,15 +1,25 @@
-import { Metadata } from "next";
+"use client";
 import DashboardShell from "@/components/DashboardShell";
-
-export const metadata: Metadata = {
-	title: "Admin Dashboard",
-	description: "Admin dashboard for e-commerce platform",
-};
+import PageLoader from "@/components/Loader";
+import { useEffect, useState } from "react";
 
 export default function DashboardLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		// Simulate a delay to mimic data fetching or server-side processing
+		const timeout = setTimeout(() => {
+			setLoading(false);
+		}, 2000); // Adjust the time as needed
+
+		return () => clearTimeout(timeout); // Cleanup timeout when the component unmounts
+	}, []);
+
+	if (loading) {
+		return <PageLoader />;
+	}
 	return <DashboardShell>{children}</DashboardShell>;
 }
